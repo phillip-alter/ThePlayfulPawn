e45b76df36067ed5e98f9d47883ff8c2cb8f3c7e
Phillip Alter MBP <phillip.alter@outlook.com>
2025-02-26 15:15:08 -0800

    Adding automatically created/edited files

--- obj/Debug/net8.0/ThePlayfulPawn.AssemblyInfo.cs
+++ obj/Debug/net8.0/ThePlayfulPawn.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThePlayfulPawn")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d071139ed1b7bc446fe9d6ab13c6eb5256c9ec3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5699ccfad2d96d9bded5b7977475aedadea63417")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThePlayfulPawn")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThePlayfulPawn")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
