238d1fa38bdb728c492e4c118ca5275f0650f845
Phillip Alter MBP <phillip.alter@outlook.com>
2025-02-26 15:13:49 -0800

    Updated to represent proper class names

--- Models/PawnDbContext.cs
+++ Models/PawnDbContext.cs
@@ -5,11 +5,11 @@ namespace ThePlayfulPawn.Data
     public class PawnDbContext : DbContext
     {
         public PawnDbContext(DbContextOptions<PawnDbContext> options) : base(options){}
-        public DbSet<Reservations> Reservations { get; set; }
-        public DbSet<Games> Games { get; set; }
-        public DbSet<Customers> Customers { get; set; }
-        public DbSet<Vendors> Vendors { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Vendor> Vendors { get; set; }
         public DbSet<Food> Foods { get; set; }
-        public DbSet<Addresses> Addresses { get; set; }
+        public DbSet<Address> Addresses { get; set; }
     }
 }
