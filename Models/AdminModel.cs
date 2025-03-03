750653d1a4a1ef1a5bb0acbcb8b0ee5236be2974
Sunair Iqbal <siqbal@my.chemeketa.edu>
2025-03-03 12:48:39 -0800

    working admin page, customer lookup working

---
+++ Models/AdminModel.cs
@@ -0,0 +1,24 @@
+using ThePlayfulPawn.Data;
+using System.Linq;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ThePlayfulPawn.Models
+{
+    public class AdminModel
+    {
+        public IEnumerable<Customer> Customers { get; set; } = new List<Customer>();
+        public IEnumerable<Address> Addresses { get; set; } = new List<Address>();
+        public IEnumerable<Vendor> Vendors { get; set; } = new List<Vendor>();
+        public IEnumerable<Food> Foods { get; set; } = new List<Food>();
+
+        private PawnDbContext _context {get; set;}
+
+
+        //context is the database that we are pulling the information from
+        public AdminModel(PawnDbContext context)
+        {
+            _context = context;
+            Customers = _context.Customers.ToList();
+        }
+    }
+}
