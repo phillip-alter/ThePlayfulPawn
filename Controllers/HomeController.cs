4a31bca294fae3d143daf12876b2798449939447
Julian Aguilar-Tomas <julianaguilar.tomas17@gmail.com>
2025-03-05 14:05:24 -0800

    Implemented GET and POST methods for Reservations page: added search functionality and new reservation creation

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using ThePlayfulPawn.Models;
 using ThePlayfulPawn.Data;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Routing.Constraints;
 
 namespace ThePlayfulPawn.Controllers;
 
@@ -39,12 +40,74 @@ public class HomeController : Controller
     {
         return View();
     }
-
-    public IActionResult Reservations()
+    /*------------------------------------------------------------------------------------------------------*/
+    // This action method handles the GET request for the Reservations page.
+    // It supports searching for reservations by the customer's first and last name.
+    [HttpGet]
+    public IActionResult Reservations(string? searchFName, string? searchLName, DateTime? searchDate)
     {
-        return View();
-    }
+        // It creates a model to hold the reservations
+        var model = new ReservationsModel();
 
+        // Gets all reservations with customer details
+        var query = from r in _context.Reservations
+                    join c in _context.Customers on r.CustomerId equals c.CustomerId
+                    select new ReservationsInputModel
+                    {
+                        CustomerFName = c.FirstName,
+                        CustomerLName = c.LastName,
+                        GroupTotal = r.GroupTotal,
+                        DateTime = r.DateTime
+                    };
+
+        // Filter by first and last name if provided
+        if (!string.IsNullOrEmpty(searchFName))
+        {
+            query = query.Where(res => res.CustomerFName.Contains(searchFName));
+        }
+
+        if (!string.IsNullOrEmpty(searchLName))
+        {
+            query = query.Where(res => res.CustomerLName.Contains(searchLName));
+        }
+
+        // If a date is search that is provided thne it will filter by reservation date
+        if(searchDate.HasValue){
+            query = query.Where(res => res.DateTime.Date == searchDate.Value.Date);
+        }
+
+        model.Reservations = query.ToList();
+        return View(model);
+    }
+    // This POST method handles adding a new reservation. 
+    // It accepts the form inputs: First Name, Last Name, Reservation Date/Time, and Group Size.
+    [HttpPost]
+    public IActionResult Reservations(string fname, string lname, DateTime datetime, int grouptotal)
+    {
+        var customer = _context.Customers.FirstOrDefault(c => c.FirstName == fname && c.LastName == lname);
+
+        if (customer == null)
+        {
+            // If the customer doesn't exist, display an error message
+            ModelState.AddModelError(string.Empty, "Customer does not exist!");
+            return View("Reservations", new ReservationsModel());
+        }
+
+        // Create a new reservation and add it to the database
+        var reservation = new Reservation
+        {
+            CustomerId = customer.CustomerId,
+            GroupTotal = grouptotal,
+            DateTime = datetime
+        };
+
+        _context.Reservations.Add(reservation);
+        _context.SaveChanges();
+
+        // Redirect to the GET action to show the updated list
+        return RedirectToAction("Reservations");
+    }
+/*-------------------------------------------------------------------------------------------------- */
     public IActionResult Privacy()
     {
         return View();
